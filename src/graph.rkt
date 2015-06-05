142c3917f74752ec27d2240b28016ab3d1a391f0
Joshua Sanchez <joshlegend26@gmail.com>
2015-06-05 10:27:31 -0500

    Cleaned up source code in graph.rkt and added new backup for riskFunctions

--- src/graph.rkt
+++ src/graph.rkt
@@ -43,72 +43,20 @@
               (slope x1 y1 x2 y2))
              )
   )
- 
+
 
 (check-expect (calc-angle 0 0 -7 0)
               0)
 
 (check-expect (calc-angle 3 -1 4 -2)
-             -45)
+              -45)
 
 (check-expect (calc-angle 0 0 -6 5)
               -40)
 
 
 
-;in-ellipse?: number(x1) number(y1) number(x2) number(y2) number (rx) number (ry) -> boolean
-;Determines whether 
-;Mouse is second coordinate pair, static point is first pair.
-;r-x and r-y are the vertical and horizontal length of the ellipse.
-;The angle-of-skew is used to calculate points if an ellipse is not aligned within an axis.
 
-;NOTE TO SELF: Replace h-k for x2 y2 with the top point of triangle
-#|(define (in-ellipse? x1 y1 x2 y2 mousex mousey r-x r-y)
-  (<=
-   (+
-    (/
-     (sqr
-      (-
-       (+
-        (* mousex
-           (cos (calc-angle x1 y1 x2 y2)
-                )
-           )
-        (* mousey
-           (sin (calc-angle x1 y1 x2 y2)
-                )
-           )
-        )
-       x2
-       )
-      )
-     (sqr r-x)
-     )
-    (/
-     (sqr
-      (-
-       (-
-        (* mousey
-           (cos (calc-angle x1 y1 x2 y2)
-                )
-           )
-        (* mousex
-           (sin (calc-angle x1 y1 x2 y2)
-                )
-           )
-        )
-       y2
-       )
-      )
-     (sqr r-y)
-     )
-    )
-   1
-   )
-  )
-
-;Check expects are coming for this, it just needs some polishing with IRL math
-|#
 
 
 (define (h x1 x2)
@@ -121,7 +69,8 @@
    (+ y1 y2)
    2))
 
-
+;in-ellipse?: number(x1) number(y1) number(x2) number(y2) number(x3) number(y3) number(mouse-x) number(mouse-y) -> boolean
+;Determines whether a given point is inside of a defined ellipse.
 (define (in-ellipse? 
          ;rightmost coord pair
          x1
@@ -135,66 +84,60 @@
          ;mouse posn
          x
          y)
-  (<=
-   (+
-    ;The first piece for X
-    (/
-     (sqr
-       (+
-        (* (- x (h x1 x2))
-           (cos
-            (calc-angle x1 y1 x2 y2)
+  (<= (+ ;The first piece for X
+         (/ (sqr (+
+                  (* (- x (h x1 x2))
+                     (cos
+                      (calc-angle x1 y1 x2 y2)
+                      )
+                     )
+                  (* (- y (k y1 y2))
+                     (sin
+                      (calc-angle x1 y1 x2 y2)
+                      )
+                     )
+                  )
+                 )
+            ;a
+            (sqr
+             (/
+              (distance x1 y1 x2 y2)
+              2
+              ))
             )
-           )
-        (* (- y (k y1 y2))
-           (sin
-            (calc-angle x1 y1 x2 y2)
+       ;The second piece for y
+         (/ (sqr (-
+                  (* (- y (k y1 y2))
+                     (cos (calc-angle x1 y1 x2 y2)
+                          )
+                     )
+                  (* (- x (h x1 x2))
+                     (sin (calc-angle x1 y1 x2 y2)
+                          )
+                     )
+                  )
+                 )
+        ;b
+            (sqr
+             (distance 
+              (h x1 x2)
+              (k y1 y2)
+              x3
+              y3
+              )
+             )
             )
-           )
-        )
+         )
+      ;1 is value of comparison of the point compared to the ellipse.
+      ;If the point is not within a distance less than or equal to 1 of the ellipse, the function will return false.
+      1
       )
-     ;a
-     (sqr
-      (/
-      (distance x1 y1 x2 y2)
-      2
-      ))
-     )
-    ;The second piece for y
-    (/
-     (sqr
-       (-
-        (* (- y (k y1 y2))
-           (cos (calc-angle x1 y1 x2 y2)
-                )
-           )
-        (* (- x (h x1 x2))
-           (sin (calc-angle x1 y1 x2 y2)
-                )
-           )
-        )
-       
-       )
-      
-     ;b
-     (sqr
-      (distance 
-       (h x1 x2)
-       (k y1 y2)
-       x3
-       y3
-       )
-      )
-     )
-    )
-   1
-   )
   )
 
-   (check-expect (in-ellipse? 0 0 -6 5 -1 4 1 0)
-                 #f)
+(check-expect (in-ellipse? 0 0 -6 5 -1 4 1 0)
+              #f)
 (check-expect (in-ellipse? 0 0 -6 5 -1 4 0 0)
-                 #t)
+              #f)
 (check-expect (in-ellipse? 2 0 -2 0 0 1 0 0)
               #t)
 
@@ -212,6 +155,6 @@
 (check-expect (in-ellipse? 689 578 552 480 647 495 610 518)
               #t)
 
-   
+
 
 (test)
