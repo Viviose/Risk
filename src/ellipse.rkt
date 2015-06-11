47720f448cab1778b47558b22258a5ea68f17854
Patrick Gallagher <pghahaha@cpgallagher.com>
2015-06-11 10:55:05 -0500

    Ready to fix hitbox now! Require ellipse.rkt to use the ellipse-skew function

--- src/ellipse.rkt
+++ src/ellipse.rkt
@@ -1,19 +1,22 @@
 #lang racket
 (require picturing-programs)
 (require "graph.rkt")
+(define BOARD (bitmap "imgs/board.png"))
 
 
-
-(define (build-ellipse x y)
+(define (pix-color x1 y1 x2 y2 x3 y3 x y c)
   (cond [(equal?
-          
           (in-ellipse? x1 y1 x2 y2 x3 y3 x y)
           #t)
          "black"]
-        [else "green"]))
+        [else c]))
          
 
 
-(build-image 700
-             700
-             build-ellipse)
+(define (ellipse-skew x1 y1 x2 y2 x3 y3)
+  (local
+    [(define (xy x y c)
+       (pix-color x1 y1 x2 y2 x3 y3 x y c))]
+             (map-image
+              xy
+              BOARD)))
