c51c01ead3299e501a11978471dd275086487cee
Patrick Gallagher <pghahaha@cpgallagher.com>
2015-06-26 20:14:18 -0500

    Run this little guy, Josh. Should fix everything!

---
+++ jashpls.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+ECHO ayy lmao top kek
+ECHO ayy lmao top kek
+ECHO ayy lmao top kek
+ECHO Run this batch file to get the rsound plugin
+
+PAUSE
+
+cd C:\Program Files\Racket
+
+raco pkg install rsound
+
+PAUSE
