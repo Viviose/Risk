96fdf2d4dfc9bee12862d83a58e729f5090dcaee
Edward Yang <edward.yang6771@gmail.com>
2016-01-02 18:20:58 -0600

    Remove unnecessary files, update .gitignore, revamp build system

---
+++ build.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+CD "%~dp0"
+SET racketdir=%~dp0Racket
+
+IF EXIST "%~dp0dist\windows" rmdir /s /q "%~dp0dist\windows"
+IF EXIST "%~dp0dist\windows" exit 1
+IF EXIST "%~dp0release\Argo.zip" del "%~dp0release\Risk.zip"
+IF EXIST "%~dp0release\Argo.zip" exit 1
+ECHO "Installing rsound (If not already installed)"
+"%racketdir%\raco.exe" pkg install rsound
+ECHO "Compiling Server..."
+"%racketdir%\raco.exe" exe "%~dp0src\risk.rkt"
+ECHO "Creating distro..."
+mkdir "%~dp0dist\windows"
+"%racketdir%\raco.exe" distribute "%~dp0dist\windows" "%~dp0src\risk.exe"
+del /F "%~dp0src\risk.exe" 
+ECHO "Zipping package..."
+"%~dp0Racket\Racket.exe" "compress.rkt"
+ECHO "Binaries in %~dp0\dist\windows\bin, packages in %~dp0\release"
+ECHO "Finished."
