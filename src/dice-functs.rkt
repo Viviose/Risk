1e2804ef049ca9a7526f31cd4ee9cac2e3f6672a
Joshua Sanchez <joshuaesanch@gmail.com>
2016-01-07 08:38:19 -0600

    Moving around functions in dice-functs, adding clause returns

--- src/dice-functs.rkt
+++ src/dice-functs.rkt
@@ -17,74 +17,7 @@
 
 ;__________________________________________________________________________________________________________________
 
-;die: number(roll value) string(attack or defend type) -> die
-;Stores values for die that are used in rolls.
-(define-struct die (number type) #:transparent)
-
-;roll: [List die(attack)] [List die(defend)] -> roll
-;Stores two lists, one filled with attack die and one with defense die.
-(define-struct roll (attack defend) #:transparent)
-
-;Dice functions to be called when dice are rolled in-game
-;roll-die: anything -> die
-;Rolls a die that returns a value 1-6. The input doesn't matter.
-(define (roll-die null)
-  (+ 1
-     (random 5)
-     )
-  )
-
-;roll-dice: number(armies defending/attackin) -> [Listof Numbers]
-;Creates a list of numbers that contains a value for each of the dice rolled.
-;Can take in any number.
-(define (roll-dice armies)
-  (cond [(equal? 0 armies) empty]
-        [else (cons
-               (roll-die "roll-who-cares-which")
-               (roll-dice (- armies 1))
-               )]
-        )
-  )
-
-;sort-rolls: [Listof Numbers] -> [Listof Numbers]
-;Sorts a list of numbers so that the greatest numbers will be at the front of the list, with the least at the end.
-;I.E. (sort-rolls (list 1 2 3)) -> (list 3 2 1)
-(define (sort-rolls lon)
-  (cond [(empty? lon) '()]
-        [else (cons (find-sup-inf > 0 lon)
-                    (sort-rolls (remove-max-or-min > lon))
-                    )]
-        )
-  )
-
-
-
-;random-roll: number(attacking armies) number(defending armies) -> roll
-;Creates a roll struct that contains two lists of attack and defense dice.
-(define (random-roll attackers defenders)
-  (roll (sort-rolls (roll-dice attackers))
-        (sort-rolls (roll-dice defenders))
-        )
-  )
-
-;Implements find-sup-inf: function(comparison operator) number(comparison value) [Listof Numbers] -> Number
-;determine-deaths: string(attack/defend) roll(die lists) -> number
-;Takes in a roll struct and a string to determine which deaths to count, and returns the amount of deaths sustained.
-(define (determine-deaths type roll)
-  (cond [(or (empty? (roll-attack roll)
-                     (roll-defend roll)
-                     )
-            )]
-        #|[]|#
-        [else (print "placeholder")]
-        )
-  )
-  #|
-  (cond [(equal? type "attack")]
-        [(equal? type "defend")]
-        [else (error "determine-deaths: Type parameter given is not 'attack' or 'defend.'")]
-  )|#
-#|)|#
+;Dice rendering
 
 ;defines easily editable constants for dice rendering
 ;default dice size in pixels
@@ -162,4 +95,78 @@
           (define dotcanvas (square (max 1 (- DICESIZE (* DOTSPACING 2))) "solid" dcolor))
           (define dicecircles (overlay/align-foldl dotcanvas DICECIRCLE (list-ref DOTPOSNS (- number 1))))]
   (overlay dicecircles
-           basedice)))
+           basedice)
+           )
+)
+
+;Dice logic
+
+;die: number(roll value) string(attack or defend type) -> die
+;Stores values for die that are used in rolls.
+(define-struct die (number type) #:transparent)
+
+;roll: [List die(attack)] [List die(defend)] -> roll
+;Stores two lists, one filled with attack die and one with defense die.
+(define-struct roll (attack defend) #:transparent)
+
+;Dice functions to be called when dice are rolled in-game
+;roll-die: anything -> die
+;Rolls a die that returns a value 1-6. The input doesn't matter.
+(define (roll-die null)
+  (+ 1
+     (random 5)
+     )
+  )
+
+;roll-dice: number(armies defending/attackin) -> [Listof Numbers]
+;Creates a list of numbers that contains a value for each of the dice rolled.
+;Can take in any number.
+(define (roll-dice armies)
+  (cond [(equal? 0 armies) empty]
+        [else (cons
+               (roll-die "roll-who-cares-which")
+               (roll-dice (- armies 1))
+               )]
+        )
+  )
+
+;sort-rolls: [Listof Numbers] -> [Listof Numbers]
+;Sorts a list of numbers so that the greatest numbers will be at the front of the list, with the least at the end.
+;I.E. (sort-rolls (list 1 2 3)) -> (list 3 2 1)
+(define (sort-rolls lon)
+  (cond [(empty? lon) '()]
+        [else (cons (find-sup-inf > 0 lon)
+                    (sort-rolls (remove-max-or-min > lon))
+                    )]
+        )
+  )
+
+
+
+;random-roll: number(attacking armies) number(defending armies) -> roll
+;Creates a roll struct that contains two lists of attack and defense dice.
+(define (random-roll attackers defenders)
+  (roll (sort-rolls (roll-dice attackers))
+        (sort-rolls (roll-dice defenders))
+        )
+  )
+
+;Implements find-sup-inf: function(comparison operator) number(comparison value) [Listof Numbers] -> Number
+;determine-deaths: string(attack/defend) roll(die lists) -> number
+;Takes in a roll struct and a string to determine which deaths to count, and returns the amount of deaths sustained.
+(define (determine-deaths type roll)
+  (cond [(or (empty? (roll-attack roll)
+                     (roll-defend roll)
+                     )
+            )
+          0]
+        #|[]|#
+        [else (print "placeholder")]
+        )
+  )
+  #|
+  (cond [(equal? type "attack")]
+        [(equal? type "defend")]
+        [else (error "determine-deaths: Type parameter given is not 'attack' or 'defend.'")]
+  )|#
+#|)|#
